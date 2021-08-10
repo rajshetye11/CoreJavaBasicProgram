603e18a501ad6bb6a79291326a4d6b32d4fae373
rajshetye11 <rajshetye11@gmail.com>
2021-08-10 23:31:24 +0530

    [raj] : added Leap Year Program

--- src/com/bridgelabz/basicprograms/LeapYear.java
+++ src/com/bridgelabz/basicprograms/LeapYear.java
@@ -9,15 +9,26 @@ public class LeapYear
 		Scanner sc= new Scanner(System.in);
 		System.out.println("Enter Year");
 		
-		int year = sc.nextInt();
 		
-		if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
-		{
-			System.out.println(year+" is a Leap Year");
-		}else
-		{
-			System.out.println(year+" is not a Leap Year");
+		for (int i=1;i<=9999;i++) {
+			
+			int year = sc.nextInt();
+			if(year<=9999 && year>=1000)
+			{
+				if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+				{
+					System.out.println(year+" is a Leap Year");
+				}else
+				{
+					System.out.println(year+" is not a Leap Year");
+				}
+			}else {
+				System.out.println("Number should be 4 digit");
+				
+			}
 		}
+		
+		
 
 	}
 }
