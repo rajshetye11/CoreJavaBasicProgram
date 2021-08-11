284de8234d94939cbf316de2710e373eb913d6bb
rajshetye11 <rajshetye11@gmail.com>
2021-08-12 00:12:55 +0530

    [raj] : added Vending Machine Solution

--- src/com/bridgelabz/junit/VendingMachine.java
+++ src/com/bridgelabz/junit/VendingMachine.java
@@ -6,25 +6,19 @@ public class VendingMachine
 {
 	public static void main(String[] args) {
 
-
+		int notes[] = new int[] {1000,500,100,50,10,5,2,1};
+		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter change you want from vending machine: ");
-		int amount = sc.nextInt();
+		System.out.println("Enter the amount of Change");
+		int change = sc.nextInt();
 		
-		
-		int[] arr = new int[]{1000,500,100,50,10,5,2,1};
-		int[] change = new int[]{0,0,0,0,0,0,0,0};
-		
-		for(int i = 0 ; i < arr.length ; i++ ) {
-			change[i] = amount/arr[i];
-			amount -= change[i]*arr[i];
-		}
-		
-		for (int i = 0 ; i< arr.length ; i++) {
-			if ( change[i] != 0 ) {
-				System.out.println(change[i] + " notes of " + arr[i]);
-			}
-		}
-	
+		System.out.println("Amount of 1000 "+ change/1000);
+		System.out.println("Amount of 500 " +(change%1000)/500);
+		System.out.println("Amount of 100 " +((change%1000)%500)/100);
+		System.out.println("Amount of 50 " +(((change%1000)%500)%100)/50);
+		System.out.println("Amount of 10 " +((((change%1000)%500)%100)%50)/10);
+		System.out.println("Amount of 5 " +(((((change%1000)%500)%100)%50)%10)/5);
+		System.out.println("Amount of 2 " +((((((change%1000)%500)%100)%50)%10)%5)/2);
+		System.out.println("Amount of 1 " +(((((((change%1000)%500)%100)%50)%10)%5)%2)/1);
 	}
 }
